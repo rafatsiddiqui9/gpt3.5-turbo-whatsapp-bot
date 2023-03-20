7e58524a70bcb71852c51995bc0719eac38daabd
Rafat Siddiqui <74584041+rafatsiddiqui9@users.noreply.github.com>
2023-03-20 10:41:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
-from chatgpt import GPT
+from flask import Flask
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "1324r3f"
 
-
-@app.route("/bot_gpt", methods=["POST"])
-def bot_gpt():
-
-    chat_model = GPT(str(request.values.get("WaId", "")))
-    incoming_msg = request.values.get("Body", "")
-    answer = chat_model.bot(incoming_msg)
-    resp = MessagingResponse()
-    msg = resp.message()
-    msg.body(answer)
-
-    return str(resp)
-
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run()
